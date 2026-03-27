3ee16872ae99678a2979ebb98591071a98311b1b
Emmazy001 <emmanuelalagbetofumi45@gmail.com>
2026-03-27 14:06:45 +0100

    test file

---
+++ newcode.py
@@ -0,0 +1,19 @@
+lst = [3, 8, 5, 2, 9, 1]
+
+def sort(lst):
+    for i in range(len(lst) - 1,0,-1):
+        for j in range(i):
+            if lst[j] > lst[j + 1]:
+                temp = lst[j]
+                lst[j] = lst[j + 1]
+                lst[j + 1] = temp
+
+
+sort(lst)
+print(lst)
+
+
+
+
+
+
